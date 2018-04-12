ebd3c028a2121b1ee33404dc8c4e1e49112a7a8c
ajt986 <asa.thibodeau@uconn.edu>
2018-04-12 15:43:05 -0400

    Added confusion matrix method.

--- Scripts/Python/peakoverlap.py
+++ Scripts/Python/peakoverlap.py
@@ -444,3 +444,151 @@ def getStrictConsensusPeaks(data, chridx=0, startidx=1, endidx=2):
             
     return np.array(allchrstrictpeaks, dtype=object)
 
+
+def getAnnotationConfusionMatrix(data1, sorted1, data2, sorted2, 
+                                 chr1idx=0, start1idx=1, end1idx=2, ann1idx=3,
+                                 chr2idx=0, start2idx=1, end2idx=2, ann2idx=3):
+    """Returns a confusion matrix of annotations between two sets of
+        peaks at the base pair level.
+    
+    Time Complexity: O(n) n=#peaks
+
+    Parameters
+    ----------
+    data1 : array-like
+       Numpy array containing the peak data with annotations.
+        
+    sorted1 : dict
+        Sort information for data1.
+        
+    data2 : array-like
+       Numpy array containing the peak data with annotations.
+        
+    sorted2 : dict
+        Sort information for data2.
+        
+    chr1idx : int
+        The chromsome index of the data1 parameter. Default: 0    
+        
+    start1idx : int
+        The start index of the data1 parameter. Default: 1
+        
+    end1idx : int
+        The end index of the data1 parameter. Default: 2
+        
+    ann1idx : int
+        The annotation index of the data1 parameter. Default: 3
+        
+    chr2idx : int
+        The chromsome index of the data2 parameter. Default: 0    
+        
+    start2idx : int
+        The start index of the data2 parameter. Default: 1
+        
+    end2idx : int
+        The end index of the data2 parameter. Default: 2
+        
+    ann2idx : int
+        The annotation index of the data2 parameter. Default: 3
+        
+    Returns
+    -------
+    m : arraylike, shape (n_unique_annotations1+1,n_unique_annotations2+1)
+       A numpy array representing a confusion matrix between unique
+       annotations among the two datasets to compare. An additional row
+       and column is included for labels and unannotated regions.
+    
+    rowlabels : list
+        The row labels of m (corresponding to data1).
+    
+    collabels : list
+        The column labels of m (corresponding to data2).
+    """
+    
+    ua1 = np.unique(data1[:, ann1idx])
+    ua2 = np.unique(data2[:, ann2idx])
+    chromosomes = np.union1d(sorted1.keys(),sorted1.keys())
+    ua1l = len(ua1)
+    ua2l = len(ua2)
+    m = np.zeros((ua1l+1, ua2l+1), dtype=np.int64)
+    rowlabels = list(ua1)
+    rowlabels.append("Unannotated")
+    collabels = list(ua2)
+    collabels.append("Unannotated")
+    
+    for curchr in chromosomes:
+        if curchr in sorted1 and curchr in sorted2:
+            cursort1 = data1[list(sorted1[curchr][:,1]),:]
+            cursort2 = data2[list(sorted2[curchr][:,1]),:]
+            
+            i1 = 0
+            i2 = 0
+            curpos = min(cursort1[i1,start1idx], cursort2[i1,start2idx])
+            while(i1 < len(cursort1) and i2 < len(cursort2)):
+                s1 = cursort1[i1,start1idx]
+                e1 = cursort1[i1,end1idx]
+                rowidx = np.where(ua1 == cursort1[i1,ann1idx])
+                
+                s2 = cursort2[i2,start2idx]
+                e2 = cursort2[i2,end2idx]
+                colidx = np.where(ua2 == cursort2[i2,ann2idx])
+                
+                maxstart = max(s1,s2)
+                minend = min(e1,e2)
+                
+                prefix = maxstart-curpos
+                if s1 < s2:
+                    m[rowidx,ua2l] = m[rowidx,ua2l]+prefix
+                elif s2 < s1:
+                    m[ua1l,colidx] = m[ua1l,colidx]+prefix
+
+                overlap = minend-maxstart+1
+                m[rowidx,colidx] = m[rowidx,colidx]+overlap
+                
+                if e1 == minend:
+                    i1 = i1+1
+                if e2 == minend:
+                    i2 = i2+1
+                curpos = minend+1
+                
+            if e1 > minend:
+                suffix = e1-curpos+1
+                m[rowidx,ua2l] = m[rowidx,ua2l]+suffix
+                i1 = i1+1
+            elif e2 > minend:
+                suffix = e2-curpos+1
+                m[ua1l,colidx] = m[ua1l,colidx]+suffix
+                i2 = i2+1
+                
+                
+            while(i1 < len(cursort1)):
+                pl = cursort1[i1,end1idx]-cursort1[i1,start1idx]+1 #assumes position format
+                rowidx = np.where(ua1 == cursort1[i1,ann1idx])
+                colidx = ua2l
+                m[rowidx, colidx] = m[rowidx, colidx]+pl
+                i1 = i1+1
+                
+            while(i2 < len(cursort2)):
+                pl = cursort2[i2,end2idx]-cursort2[i2,start2idx]+1 #assumes position format
+                rowidx = ua1l
+                colidx = np.where(ua2 == cursort2[i2,ann2idx])
+                m[rowidx, colidx] = m[rowidx, colidx]+pl
+                i2 = i2+1
+            
+        else:
+            if curchr in sorted1:
+                cursort1 = data1[list(sorted1[curchr][:,1]),:]
+                for j in range(0, len(cursort1)):
+                    pl = cursort1[j,end1idx]-cursort1[j,start1idx]+1 #assumes position format
+                    rowidx = np.where(ua1 == cursort1[j,ann1idx])
+                    colidx = ua2l
+                    m[rowidx, colidx] = m[rowidx, colidx]+pl
+            else:
+                cursort2 = data2[list(sorted2[curchr][:,1]),:]
+                for j in range(0, len(cursort2)):
+                    pl = cursort2[j,end2idx]-cursort2[j,start2idx]+1 #assumes position format
+                    rowidx = ua1l
+                    colidx = np.where(ua2 == cursort2[j,ann2idx])
+                    m[rowidx, colidx] = m[rowidx, colidx]+pl
+                    
+    return m, rowlabels, collabels
